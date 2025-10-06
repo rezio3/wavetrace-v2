544b43b40e7dbf39809f1cea97346b5dfbf58550
Jakub Rezler <jakub.rezler96@gmail.com>
2025-10-07 01:53:38 +0200

    update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,29 +10,33 @@ const Footer = () => {
     <SectionWrapper className="justify-content-between align-items-end mb-4 mt-5">
       <span className="text-secondary">Wavetrace &copy; 2024</span>
       <div className="position-absolute b-0 socials-container">
-        <Button variant="text" className="social-icon">
-          <InstagramIcon />
-        </Button>
-        <Button variant="text" className="social-icon">
-          <XIcon />
-        </Button>
-        <Button variant="text" className="social-icon">
-          <YouTubeIcon />
-        </Button>
+        {socialIcons.map((e) => (
+          <Button variant="text" className="social-icon">
+            {e.icon}
+          </Button>
+        ))}
       </div>
       <div>
-        <Button variant="text" data-pageindex={1} onClick={() => {}}>
-          Pricing
-        </Button>
-        <Button variant="text" data-pageindex={1} onClick={() => {}}>
-          FAQ
-        </Button>
-        <Button variant="text" data-pageindex={1} onClick={() => {}}>
-          Support
-        </Button>
+        {footerLinks.map((e) => (
+          <Button variant="text" data-pageindex={1} onClick={() => {}}>
+            {e.linkText}
+          </Button>
+        ))}
       </div>
     </SectionWrapper>
   );
 };
 
 export default Footer;
+
+const socialIcons = [
+  { icon: <InstagramIcon />, link: "" },
+  { icon: <XIcon />, link: "" },
+  { icon: <YouTubeIcon />, link: "" },
+];
+
+const footerLinks = [
+  { linkText: "Pricing", link: "" },
+  { linkText: "FAQ", link: "" },
+  { linkText: "Support", link: "" },
+];
