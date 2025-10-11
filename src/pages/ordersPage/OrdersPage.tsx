bf43ac864b1f43047cb064e3f8cbe9c732158222
Jakub Rezler <jakub.rezler96@gmail.com>
2025-10-11 02:03:45 +0200

    add header to orders page

--- src/pages/ordersPage/OrdersPage.tsx
+++ src/pages/ordersPage/OrdersPage.tsx
@@ -1,11 +1,27 @@
-import SectionWrapper from "../../components/SectionWrapper";
+import SectionWrapper from "../../components/elements/SectionWrapper";
 import OrderForm from "../../components/ordersPage/OrderForm";
+import HeaderText from "../../components/elements/HeaderText";
 
 const OrdersPage = () => {
   return (
-    <SectionWrapper centered>
-      <OrderForm />
-    </SectionWrapper>
+    <>
+      <SectionWrapper className="d-flex justify-content-center">
+        <HeaderText
+          headerType="h2"
+          fontSize={40}
+          fontFamily="Carlito"
+          fontWeight={700}
+          letterSpacing={5}
+          className="my-5 py-2"
+        >
+          Order your custom track
+        </HeaderText>
+      </SectionWrapper>
+
+      <SectionWrapper className="align-items-start">
+        <OrderForm />
+      </SectionWrapper>
+    </>
   );
 };
 
