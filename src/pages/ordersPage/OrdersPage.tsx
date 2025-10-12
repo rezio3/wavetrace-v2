ab7af67526b7ae94a9a251d3bd45685af6b64da1
Jakub Rezler <jakub.rezler96@gmail.com>
2025-10-12 02:03:05 +0200

    update order page text & add Roboto font

--- src/pages/ordersPage/OrdersPage.tsx
+++ src/pages/ordersPage/OrdersPage.tsx
@@ -1,21 +1,38 @@
 import SectionWrapper from "../../components/elements/SectionWrapper";
 import OrderForm from "../../components/ordersPage/OrderForm";
 import HeaderText from "../../components/elements/HeaderText";
+import CustomText from "../../components/elements/CustomText";
 
 const OrdersPage = () => {
   return (
     <>
-      <SectionWrapper className="d-flex justify-content-center">
+      <SectionWrapper centered className="align-items-center">
         <HeaderText
           headerType="h2"
           fontSize={40}
           fontFamily="Carlito"
           fontWeight={700}
-          letterSpacing={5}
-          className="my-5 py-2"
+          letterSpacing={3}
+          className="mt-5"
         >
           Order your custom track
         </HeaderText>
+        <CustomText
+          fontWeight={200}
+          fontSize={20}
+          fontFamily={"Roboto, sans-serif"}
+          className="text-center"
+        >
+          Describe the style of music your project requires.
+        </CustomText>
+        <CustomText
+          fontWeight={100}
+          fontSize={16}
+          fontFamily={"Roboto, sans-serif"}
+          className="text-center mb-5"
+        >
+          We’ll reply as quickly as we can.
+        </CustomText>
       </SectionWrapper>
 
       <SectionWrapper className="align-items-start">
