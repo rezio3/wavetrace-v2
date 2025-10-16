09d30ff4313521829792f0e138987755bc14ff0a
Jakub Rezler <jakub.rezler96@gmail.com>
2025-10-17 01:12:29 +0200

    feat / adjust HeaderText to take ref from MUI Tooltip

--- src/components/elements/HeaderText.tsx
+++ src/components/elements/HeaderText.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 type FontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
 
 type HeaderTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
@@ -13,31 +15,39 @@ type HeaderTextProps = {
   width?: string;
 };
 
-const HeaderText: React.FC<HeaderTextProps> = ({
-  children,
-  className = "",
-  fontSize = 45,
-  fontFamily = "Carlito",
-  fontWeight = 400,
-  letterSpacing = 1,
-  headerType = "h2",
-  width = "auto",
-}) => {
-  const Tag = headerType;
-  return (
-    <Tag
-      className={`m-0 ${className} `}
-      style={{
-        fontSize: fontSize,
-        fontFamily: fontFamily,
-        fontWeight: fontWeight,
-        letterSpacing: letterSpacing,
-        width: width,
-      }}
-    >
-      {children}
-    </Tag>
-  );
-};
+const HeaderText = React.forwardRef<HTMLHeadingElement, HeaderTextProps>(
+  (
+    {
+      children,
+      className = "",
+      fontSize = 45,
+      fontFamily = "Carlito",
+      fontWeight = 400,
+      letterSpacing = 1,
+      headerType = "h2",
+      width = "auto",
+      ...tooltipRest
+    },
+    ref
+  ) => {
+    const Tag = headerType;
+    return (
+      <Tag
+        ref={ref}
+        className={`m-0 ${className}`}
+        style={{
+          fontSize,
+          fontFamily,
+          fontWeight,
+          letterSpacing,
+          width,
+        }}
+        {...tooltipRest}
+      >
+        {children}
+      </Tag>
+    );
+  }
+);
 
 export default HeaderText;
