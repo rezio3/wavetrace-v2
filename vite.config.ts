865246e1bb3b54f07787928a1e43719c3988a33a
Jakub Rezler <jakub.rezler96@gmail.com>
2025-10-17 13:48:44 +0200

    update production base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/wavetrace-v2/",
+  // base: "/wavetrace-v2/",
+  base: "/",
+  // ^^^^^^^^^^^^^ production base ^^^^^^^^^^^^^^^
 });
