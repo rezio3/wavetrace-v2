a29353ba6c9a7662a7a88234de3b3c1ab3913956
Jakub Rezler <jakub.rezler96@gmail.com>
2025-10-17 22:04:01 +0200

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/",
+  base: "/",
   // ^^^^^^^^^^^^^ production base ^^^^^^^^^^^^^^^
-  base: "/wavetrace-v2/",
+  // base: "/wavetrace-v2/",
 });
